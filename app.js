17a8f4c2cb9eb651374cf85fe6db84ecc4cf3817
MichaelRempe <michaeljrempe@gmail.com>
2020-02-09 18:10:24 -0500

    inital DB view cases established

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 let inquirer = require("inquirer");
 let CRUD = require("./db_CRUD");
 
-let main = ()=>{
+let main = () => {
     console.log("--------- Content Management System----------\n          ----- Version 1.0.0 -----     ")
     start();
 }
 // INITIAL DATABASE CHOICES
-let start = ()=>{
+let start = () => {
     inquirer.prompt([
         {
-            message:"What would you like to do?",
+            message: "What would you like to do?",
             type: "list",
             choices: ["ADD to Database", "MODIFY Database", "VIEW Database", "EXIT"],
             name: "choice"
         }
-    ]).then((resp)=>{
-        switch(resp.choice){
+    ]).then((resp) => {
+        switch (resp.choice) {
             case resp.choice = "ADD to Database":
                 ADD();
                 break;
@@ -34,7 +34,7 @@ let start = ()=>{
     })
 };
 //  DATABASE OPERATION CHOICES
-let ADD = ()=>{
+let ADD = () => {
     inquirer.prompt([
         {
             message: "What would you like to add?",
@@ -42,8 +42,8 @@ let ADD = ()=>{
             choices: ["Department", "Role", "Employee", "Return"],
             name: "choice"
         }
-    ]).then((resp)=>{
-        switch(resp.choice){
+    ]).then((resp) => {
+        switch (resp.choice) {
             case resp.choice = "Department":
                 console.log("1")
                 break;
@@ -61,17 +61,17 @@ let ADD = ()=>{
         }
     });
 };
-let MODIFY = ()=>{
+let MODIFY = () => {
     // let _employees = getEmployees().then((res)=>{console.log(res)}).catch((err)=>{console.log(err)});
     getEmployees();
     CRUD.endConnection();
-        
+
     // .catch((err)=>{console.log(err)})
     // prompt which employee you want to update
     //CRUD update call
-    
+
 };
-let VIEW = ()=>{
+let VIEW = () => {
     inquirer.prompt([
         {
             message: "What would you like to view?",
@@ -79,31 +79,51 @@ let VIEW = ()=>{
             choices: ["Departments", "Roles", "Employees", "Return"],
             name: "choice"
         }
-    ]).then((resp)=>{
-        switch(resp.choice){
-            case resp.choice = "Departments":
-                console.log("1")
-                break;
-            case resp.choice = "Roles":
-                console.log("2")
-                break;
-            case resp.choice = "Employees":
-                console.log("3")
-                break;
-            case resp.choice = "Return":
-                start();
-                break;
-            default:
-                console.log("Error occurred, please try again or enter: ctrl^c to exit.");
+    ]).then((resp) => {
+        if(resp.choice === "Return"){
+            start();
+        }else{
+            //NEEDS TO BE ASYNC AWAIT BEFORE RETURNING TO INITIAL OPTION LIST
+            CRUD.view(resp.choice);
+            start();
         }
+        // switch (resp.choice) {
+        //     case resp.choice = "Departments":
+        //         console.log("1")
+        //         break;
+        //     case resp.choice = "Roles":
+        //         console.log("2")
+        //         break;
+        //     case resp.choice = "Employees":
+        //         console.log("3")
+        //         break;
+        //     case resp.choice = "Return":
+        //         start();
+        //         break;
+        //     default:
+        //         console.log("Error occurred, please try again or enter: ctrl^c to exit.");
+        // }
     });
 };
-let EXIT = ()=>{
+let EXIT = () => {
     CRUD.endConnection();
     console.log(" ------------- Exiting CMS ------------- ")
 };
+
+// CRUD INQUIRIES
+
+let addEmployee = () => { };
+let addRole = () => { };
+let addDepartment = () => { };
+
+let viewByDepartment = () => {
+
+};
+let viewByRole = () => { };
+let viewEmployees = () => { };
+
 //ASYNC FUNCTIONS
-let getEmployees = async ()=>{
+let getEmployees = async () => {
     let employees = await CRUD.getEmployees();
     console.log(employees);
 }
--- db_CRUD.js
+++ db_CRUD.js
@@ -15,11 +15,41 @@ connection.connect(function (err) {
     if (err) throw err;
     // console.log("connected as id " + connection.threadId);
 });
-let endConnection = ()=>{
+let endConnection = () => {
     connection.end();
 }
 
 //CRUD OPERATIONS
+
+//Views
+let view = (table) => {
+    //Format input to match table names
+    table = table.toLowerCase(); 
+    table = table.substring(0, table.length - 1);
+    
+    let query = connection.query(
+        "SELECT * FROM ?", [table],
+        (err, res)=>{
+            switch(table){
+                case table = "department":
+                    console.log("Depart data");
+                    break;
+                case table = "role":
+                    console.log("role data");
+                    break;
+                case table = "employee":
+                    console.log("employee data")
+                    break;
+                default:
+                    console.log("An Error occurred, hit ctrl^C to escape");
+            }
+        }
+
+    );
+}
+
+
+
 let getEmployees = () => {
     let employees = [];
     let query = connection.query(
@@ -35,6 +65,7 @@ let getEmployees = () => {
 }
 //EXPORT OPERATIONS
 module.exports = {
-    endConnection : endConnection,
-    getEmployees : getEmployees,
+    endConnection: endConnection,
+    getEmployees: getEmployees,
+    view: view,
 }
